b2a7fb5bcc5da717908645fde4b0610baddbb91e
httpassoca <trabsom.rafael@gmail.com>
2021-12-11 03:27:50 -0300

    refactor(vuex): removing constants mutation types because isnt valid on small projects

--- src/store/user/mutations.ts
+++ src/store/user/mutations.ts
@@ -2,13 +2,8 @@ import Vue from 'vue';
 import { MutationTree } from 'vuex';
 import { ModuleState, User } from './types';
 
-export enum UserMutations {
-  SET_USER = 'SET_USER',
-  REMOVE_USER = 'REMOVE_USER',
-}
-
 const mutations: MutationTree<ModuleState> = {
-  [UserMutations.SET_USER](state, payload: User) {
+  SET_USER(state, payload: User) {
     if (payload) {
       Vue.set(state, 'user', payload);
     } else {
